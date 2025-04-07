ddf5dc75ff7e55dbf9019f29056afc68637a91de
Usama0444 <rajausama0444@gmail.com>
2025-04-07 16:03:05 +0500

    working start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:savrly/screens/admin/admin_panel.dart';
-
 import 'auth/login/login_screen.dart';
 
 void main() {
