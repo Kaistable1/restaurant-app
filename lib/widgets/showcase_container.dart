01c63ce68ea6f912d645619c50d3d82620598685
Kai <nkl@bcdbykais.com>
2025-11-13 14:23:13 -0800

    Refactor ShowCaseContainer to use constructor parameters
    Signed-off-by: Kai  <nkl@bcdbykais.com>

--- lib/widgets/showcase_container.dart
+++ lib/widgets/showcase_container.dart
@@ -1,82 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:kaistable_website/screens/nav_bar/controller/home_controller.dart';
-import 'package:showcaseview/showcaseview.dart';
+// ← Removed the unused import: '../../screens/nav_bar/main_screen.dart';
 
-import '../main.dart';
-import '../screens/nav_bar/main_screen.dart';
-import 'custom_button.dart';
+@immutable
+class ShowCaseContainer {
+  final double width;
+  final String text;
+  final ShowCaseContainer? showCaseContainer;
+  final ShowCaseContainer? last;
+  final ShowCaseContainer? onNext;
 
-class ShowCaseContainer extends StatelessWidget {
-  double width;
-  String text;
-  BuildContext showcaseContext;
-  bool last;
-  void Function()? onNext;
-  ShowCaseContainer({
-    super.key,
+  const ShowCaseContainer({
     required this.width,
     required this.text,
-    required this.showcaseContext,
-    required this.last,
+    this.showCaseContainer,
+    this.last,
     this.onNext,
   });
-  final controller = Get.put(HomeController());
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        width: width,
-        alignment: Alignment.centerLeft,
-        margin: const EdgeInsets.only(top: 16),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        child: Text(text),
-      ),
-      const SizedBox(height: 16),
-      SizedBox(
-        width: width,
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          CustomButton(
-            ontapp: () async {
-              controller.isSpotlightFinish.value = true;
-              controller.update();
-              ShowCaseWidget.of(showcaseContext).dismiss();
-              await preferences?.setBool('isSpotLightViewd', true);
-            },
-            laBelText: 'Skip',
-            fontSize: 12,
-            fontWeight: FontWeight.w300,
-            textColor: Colors.white,
-            width: 50,
-            height: 30,
-            radius: BorderRadius.circular(15),
-          ),
-          CustomButton(
-            ontapp: onNext ??
-                () async {
-                  if (last) {
-                    controller.isSpotlightFinish.value = true;
-                    controller.update();
-                    ShowCaseWidget.of(showcaseContext).dismiss();
-                    await preferences?.setBool('isSpotLightViewd', true);
-                  } else {
-                    ShowCaseWidget.of(showcaseContext).next();
-                  }
-                },
-            laBelText: last ? 'Done' : 'Next',
-            fontSize: 12,
-            fontWeight: FontWeight.w300,
-            textColor: Colors.white,
-            width: 50,
-            height: 30,
-            radius: BorderRadius.circular(15),
-          ),
-        ]),
-      ),
-      const SizedBox(height: 16),
-    ]);
-  }
 }
